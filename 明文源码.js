ace2befbaac311732ba3d094678d48e233142d06
tom5920 <162852929+tom5920@users.noreply.github.com>
2025-02-14 15:07:42 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'd6ecb809-20cc-42d9-b13c-7e67021b01d2';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
